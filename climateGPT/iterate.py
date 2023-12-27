de7a92bf9ee095a0eb770287f398826f59dc58b4
pierre <pierre.pessarossi@gmail.com>
2023-12-27 10:22:22 +0100

    Add batch iterator for pre-tokenized climate data

--- climateGPT/iterate.py
+++ climateGPT/iterate.py
@@ -93,12 +93,71 @@ class TokenIterator(torch.utils.data.IterableDataset):
             yield x, y
 
 
-if __name__ == "__main__":
-    ds = TokenIterator(Path("climateGPT/data/tok32000"), 6, "val")
+# create a dataset class to loop over batches of pre-tokenized data
+class TokenBatches(torch.utils.data.Dataset):
+    def __init__(
+        self, pretokenized_source: Path, context_length: int, split: str
+    ) -> None:
+        super().__init__()
+        self.pretokenized_source = pretokenized_source
+        self.context_length = context_length
+        self.split = split
+        total_filenames = sorted(self.pretokenized_source.glob("*.bin"))
+        self.filenames = (
+            total_filenames[4:] if self.split == "train" else total_filenames[:4]
+        )
+        if len(self.filenames) == 0:
+            raise ValueError(
+                "The pretokenized source folder does not contain any .bin files."
+            )
+        self.memmaps = [np.memmap(f, dtype=np.uint16, mode="r") for f in self.filenames]
+        self.total_batches = np.cumsum(
+            [len(m) // (self.context_length + 1) for m in self.memmaps]
+        )  # we count the cumulative number of batches in all shards
 
-    dl = torch.utils.data.DataLoader(ds, batch_size=1, pin_memory=True, num_workers=2)
+    def __len__(self):
+        return self.total_batches[-1]
+
+    def __getitem__(self, idx):
+        # Find which shard contains the desired index
+        shard_idx = np.searchsorted(self.total_batches, idx, side="right")
+        if shard_idx > 0:
+            idx -= self.total_batches[
+                shard_idx - 1
+            ]  # Adjust index relative to the shard
+
+        # Load the sample from the appropriate memmap file
+        start_token_idx = idx * (self.context_length + 1)  # +1 for the target
+        chunk = self.memmaps[shard_idx][
+            start_token_idx : start_token_idx + self.context_length + 1
+        ]
+        # calling .astype will copy the data into a new numpy array, now in RAM
+        chunk = torch.from_numpy(chunk.astype(np.int64))
+        x = chunk[:-1]
+        y = chunk[1:]
+
+        return x, y
+
+
+if __name__ == "__main__":
+    # ds = TokenIterator(Path("climateGPT/data/tok32000"), 6, "val")
+
+    # dl = torch.utils.data.DataLoader(ds, batch_size=1, pin_memory=True, num_workers=2)
+    # for i, (x, y) in enumerate(dl):
+    #     if i > 1:
+    #         break
+    #     print(x)
+    #     print(y)
+
+    ds = TokenBatches(
+        Path("climateGPT/data/fine_tuning/vocab_2000_context_512"), 512, "train"
+    )
+
+    dl = torch.utils.data.DataLoader(
+        ds, batch_size=1, pin_memory=True, num_workers=0, shuffle=True
+    )
+
     for i, (x, y) in enumerate(dl):
-        if i > 1:
+        print(len(x[0]))
+        if i > 10:
             break
-        print(x)
-        print(y)
